27adcc0e46ad94c76f8266da954903e36645d2f8
Rados Rajcic <RajcicRados@hotmail.com>
2023-01-10 19:47:05 +0100

    Updates

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -219,6 +219,7 @@ namespace WebAppOglas.Controllers
                         }
                     }
                 }
+        
                 Oglas.Izmeni(automobil);
 
                 TempData["izmenjenAuto"] = automobil.Marka +
